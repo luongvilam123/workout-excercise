5f7a6b6c5e2ec251f00833b6b074359b1729a926
LuongViLam <kulam1782000@gmail.com>
2022-06-18 13:15:48 +0700

    add components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
 root.render(
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
 );
