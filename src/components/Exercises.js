b68f7eaca3993fb5496d82ef07810b6a3719b435
LuongViLam <kulam1782000@gmail.com>
2022-06-21 16:54:53 +0700

    begin exercise components

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,6 +1,10 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, {useState,useEffect} from 'react';
+import { Pagination } from '@mui/material/Pagination';
+import {Box, Stack, Typography} from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
-const Exercises = () => {
+const Exercises = ({exercises,setExercises,bodyPart}) => {
   return (
     <div>Exercises</div>
   )
