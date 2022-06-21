e75af11e029eb000fa9e2cba8698f980adabd6b9
LuongViLam <kulam1782000@gmail.com>
2022-06-21 16:13:53 +0700

    horizontal com update

--- src/components/BodyParts.js
+++ src/components/BodyParts.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
 
-const BodyParts = () => {
+const BodyParts = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <div>BodyParts</div>
-  )
-}
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
+        backgroundColor: "#fff",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px"
+      }}
+    >
+      <img src={Icon} alt="dumbbel" style={{ width: "40px", height: "40px" }} />
+    </Stack>
+  );
+};
 
-export default BodyParts
+export default BodyParts;
